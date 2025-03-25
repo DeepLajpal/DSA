ec505e6667658776db987f55c420bae72f0db426
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-03-25 18:18:30 +0530

    Remove unused JavaScript file and add new array reversal implementation in C++

---
+++ Striver Notes/Q15_reverseArr_usingAnExtraArr.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArr(vector<int> &arr, int n)
+
+{
+    vector<int> arrReversed(n);
+    for (int i = 0; i < n; i++)
+    {
+        arrReversed[i] = arr[n - i - 1];
+        cout << i << " ";
+    }
+}
+int main()
+{
+    cout << "Hello World" << endl;
+    vector<int> arr = {1, 2, 3, 4, 5};
+    reverseArr(arr, arr.size());
+    return 0;
+}
