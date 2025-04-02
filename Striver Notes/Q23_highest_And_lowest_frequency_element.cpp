6f4a58ac3964b461cd71b7f61f77a9eac272212c
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-04-02 20:26:05 +0530

    [new question] : Q23_highest_And_lowest_frequency_element

---
+++ Striver Notes/Q23_highest_And_lowest_frequency_element.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of arr elmt you wnt: ";
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "set the arr elmt " << i << ": ";
+        cin >> arr[i];
+    }
+
+    // pre-storing
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]] += 1;
+    }
+
+    // finding highest and lowest
+    int highestFreq = INT_MIN;
+    int lowestFreq = INT_MAX;
+    int highestElem = 0;
+    int lowestElem = 0;
+
+    for (auto i : mpp)
+    {
+        if (i.second > highestFreq)
+        {
+            highestFreq = i.second;
+            highestElem = i.first;
+        }
+        if (i.second < lowestFreq)
+        {
+            lowestFreq = i.second;
+            lowestElem = i.first;
+        }
+        cout << "Element with highest frequency: " << highestElem << " (appears " << highestFreq << " times)" << endl;
+        cout << "Element with lowest frequency: " << lowestElem << " (appears " << lowestFreq << " times)" << endl;
+    }
+}
