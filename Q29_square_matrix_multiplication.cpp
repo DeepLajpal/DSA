f4f768d39b2308889bd3fa2370a6623952a10958
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-04-11 19:12:50 +0530

    Add square matrix multiplication implementation in Q29

---
+++ Q29_square_matrix_multiplication.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> getSquareMatrixMultiplication(vector<vector<int>> a, vector<vector<int>> b, int rowA, int columnA, int rowB, int columnB)
+{
+    if (columnA != rowB)
+    {
+        cout << "Wrong Input";
+        return {{-1}, {-1}};
+    };
+
+    vector<vector<int>> result(rowA, vector<int>(columnB, 0));
+    for (int i = 0; i < rowA; i++)
+    {
+        for (int j = 0; j < columnB; j++)
+        {
+            for (int k = 0; k < rowB; k++)
+            {
+                result[i][j] += (a[i][k] * b[k][j]);
+            }
+        }
+    }
+    return result;
+}
+int main()
+{
+    vector<vector<int>> a = {{2, 3, 7}, {-1, 5, 2}};
+    vector<vector<int>> b = {{3, 0}, {2, 5}, {-2, 7}};
+    vector<vector<int>> result = getSquareMatrixMultiplication(a, b, a.size(), a[0].size(), b.size(), b[0].size());
+    for (int i = 0; i < result[0].size(); i++)
+    {
+        for (int j = 0; j < result.size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
