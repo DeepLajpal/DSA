f48dc0999769afd417ebc7bbcf05eb7b56be3085
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-04-11 19:14:40 +0530

    rename the file from square_matrix_multiplication to matrix_multiplication

--- Q29_matrix_multiplication.cpp
+++ Q29_matrix_multiplication.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> getSquareMatrixMultiplication(vector<vector<int>> a, vector<vector<int>> b, int rowA, int columnA, int rowB, int columnB)
+vector<vector<int>> getMatrixMultiplication(vector<vector<int>> a, vector<vector<int>> b, int rowA, int columnA, int rowB, int columnB)
 {
     if (columnA != rowB)
     {
@@ -26,7 +26,7 @@ int main()
 {
     vector<vector<int>> a = {{2, 3, 7}, {-1, 5, 2}};
     vector<vector<int>> b = {{3, 0}, {2, 5}, {-2, 7}};
-    vector<vector<int>> result = getSquareMatrixMultiplication(a, b, a.size(), a[0].size(), b.size(), b[0].size());
+    vector<vector<int>> result = getMatrixMultiplication(a, b, a.size(), a[0].size(), b.size(), b[0].size());
     for (int i = 0; i < result[0].size(); i++)
     {
         for (int j = 0; j < result.size(); j++)
